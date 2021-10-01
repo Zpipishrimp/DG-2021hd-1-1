344f8f20579531589376a2e3298357cbf39615a9
cdle <798731886@qq.com>
2021-10-01 14:26:21 +0800

    update

---
+++ share_code.js
@@ -0,0 +1,30 @@
+var allShareCodes = [];
+var removedShareCodes = [];
+
+exports.setDefaultShareCodes = function(shareCodes) {
+    if (shareCodes && shareCodes.length) {
+        for (var shareCode of shareCodes) {
+            allShareCodes.push(shareCode)
+        }
+    }
+}
+
+exports.addShareCode = function(shareCode) {
+    if (allShareCodes.indexOf(shareCode) == -1) {
+        allShareCodes.push(shareCode)
+    }
+}
+
+exports.removeShareCode = function(shareCode) {
+    removedShareCodes.push(shareCode)
+}
+
+exports.forEachShareCode = function(func) {
+    for (var shareCode of allShareCodes) {
+        if (fullShareCodes.indexOf(shareCode) == -1) {
+            if (func(shareCode)) {
+                break
+            }
+        }
+    }
+}
